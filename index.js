615e5f0c53d4b1da5d9415d5d6d8cc1616c88388
Connell McCarthy <connellmccarthy@gmail.com>
2021-07-14 16:10:54 -0400

    Port stuff

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ var engine = new Liquid({
   extname: '.liquid'
 })
 const app = express()
-const port = 3000
+const port = normalizePort(process.env.PORT || 3000)
+app.set('port', port)
+
 
 app.engine('liquid', engine.express())
 app.set('view engine', 'liquid');
-app.set('port', process.env.PORT || port)
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
@@ -27,3 +28,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 })
+
+function normalizePort(val) {
+  var port = parseInt(val, 10)
+  if (isNaN(port)) {
+    return val
+  }
+  if (port >= 0) {
+    return port
+  }
+  return false
+}
