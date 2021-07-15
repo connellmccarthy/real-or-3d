66b12dbb6f3837856ad1363cddfa28c78e85cc2e
Connell McCarthy <connellmccarthy@gmail.com>
2021-07-15 11:37:16 -0400

    Setup login as middleware + verify for all /admin/ domains

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,32 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const sql = require('@sql')
-const {verify} = require('@auth')
+const {login, verify} = require('@auth')
 
-router.post('/login', async (req, res) => {
-  let username = req.body.username
-  let password = req.body.password
-
-  const verifyUser = await sql.query({
-    sql: "SELECT `id` FROM `admin` WHERE `username`=? AND `password`=SHA2(?, 256)",
-    values: [req.body.username, req.body.password]
-  })
-  if (verifyUser[0]) {
-    console.log('Authenticated!')
-    req.session.admin_auth = true
-    res.redirect('/admin')
-  } else {
-    console.log('Not found')
-    res.redirect('/admin/login')
-  }
+router.post('/login', login, async (req, res) => {
+  res.redirect('/admin')
 })
 
 router.get('/login', (req, res) => {
   res.render('admin/login')
-  console.log(req.session.admin_auth)
 })
 
-router.get('/', verify, (req, res) => {
+router.use('/*', verify, (req, res, next) => {
+  next()
+})
+
+router.get('/', (req, res) => {
   res.send('Authenticated!')
 })
 
