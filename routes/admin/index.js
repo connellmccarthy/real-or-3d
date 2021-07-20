7dacdfdbed6cbc96606c112839cd4775fefe662b
Connell McCarthy <connellmccarthy@gmail.com>
2021-07-19 22:23:37 -0400

    More admin stuff

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const sql = require('@sql')
 const {login, verify} = require('@auth')
+const cloudinary = require('@cloudinary')
 
 router.post('/login', login, async (req, res) => {
   res.redirect('/admin')
@@ -10,7 +11,7 @@ router.post('/login', login, async (req, res) => {
 router.get('/login', (req, res) => {
   res.render('admin/login', {
     page: {
-      title: 'Admin',
+      title: 'Login',
       toast: req.flash()
     }
   })
@@ -23,10 +24,46 @@ router.use('/*', verify, (req, res, next) => {
 router.get('/', (req, res) => {
   res.render('admin/index',  {
     page: {
-      title: 'Admin',
+      title: 'Dashboard',
       toast: req.flash()
     }
   })
+})
+
+router.get('/new', (req, res) => {
+  res.render('admin/new', {
+    page: {
+      title: "New challenge",
+      toast: req.flash()
+    }
+  })
+})
+
+router.post('/new', async (req, res) => {
+  let slug = req.body.title
+  slug = slug.replace(/\s+/g, '-').toLowerCase()
+  const challenge = {
+    image: req.body.image,
+    title: req.body.title,
+    slug: slug,
+    author: req.body.author,
+    season: req.body.season,
+    type: req.body.type,
+    date_post: new Date(),
+    date_open: req.body.date_open,
+    date_close: req.body.date_close,
+    description: req.body.description,
+    outlet: req.body.outlet
+  }
+  cloudinary.upload(challenge.image, challenge.slug)
+  console.log(challenge)
+  req.flash('successMessage', 'Successfully created challenge')
+  res.redirect('/admin')
+})
+
+router.get('/logout', (req, res) => {
+  req.session.admin_auth = false
+  res.redirect('/')
 })
 
 module.exports = router
