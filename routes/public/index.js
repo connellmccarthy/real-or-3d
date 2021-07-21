68506d77b2609bda41075ad3645fdc76a109114f
alexatshopify <alex.djordjevic@shopify.com>
2021-07-21 10:51:13 -0400

    use challenges data, change window format

--- routes/public/index.js
+++ routes/public/index.js
@@ -5,32 +5,7 @@ const api = require('@api')
 
 router.get('/', async (req, res) => {
   const users = ['@someone', '@someoneelse', '@tacos', '@pizza']
-  const challenges = [
-    {
-      name: 'challenge-01',
-      realOr3d: 'Real',
-      votesReal: 5,
-      votes3d: 7
-    },
-    {
-      name: 'challenge-02',
-      realOr3d: 'Real',
-      votesReal: 7,
-      votes3d: 5
-    },
-    {
-      name: 'challenge-03',
-      realOr3d: '3D',
-      votesReal: 6,
-      votes3d: 6
-    },
-    {
-      name: 'challenge-04',
-      realOr3d: '3D',
-      votesReal: 2,
-      votes3d: 10
-    }
-  ]
+  const challenges = await api.challenges.get()
   res.render('public/index', {
     page: {
       leaderboard: {
