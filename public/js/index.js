066b7b459d005a01c32b21e097317bf43ebca474
Connell McCarthy <connell.mccarthy@shopify.com>
2021-07-21 12:18:57 -0400

    Change + close windows

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,52 @@
-console.log('lakjsdflkj')
-var active_window;
+var a_click = new Audio('/audio/click.mp3')
 document.querySelectorAll('.window').forEach((element) => {
-  console.log(element)
   element.addEventListener('mousedown', () => {
-    if (active_window !== element) {
-      element.classList.add('active')
-      if (active_window){
-        active_window.classList.remove('active')
-      }
-      active_window = element
+    if (active_window !== element.getAttribute('data-id')) {
+      let id = element.getAttribute('data-id')
+      changeWindow(id)
     }
   })
-});
+})
+document.querySelectorAll('.tab').forEach((element) => {
+  element.addEventListener('click', () => {
+    if (active_window !== element.getAttribute('data-id')) {
+      click()
+      let id = element.getAttribute('data-id')
+      changeWindow(id)
+    }
+  })
+})
+
+document.querySelectorAll('.close').forEach((element) => {
+  element.addEventListener('click', () => {
+    click()
+    let id = element.getAttribute('data-id')
+    removeWindow(id)
+  })
+})
+
+function changeWindow(id) {
+  document.querySelector(`.window[data-id="${id}"]`).classList.add('active')
+  document.querySelector(`.tab[data-id="${id}"]`).classList.add('active')
+
+  if (active_window){
+    if (document.querySelector(`.window[data-id="${active_window}"]`)){
+      document.querySelector(`.window[data-id="${active_window}"]`).classList.remove('active')
+      document.querySelector(`.tab[data-id="${active_window}"]`).classList.remove('active')
+    }
+  }
+  active_window = id
+}
+function removeWindow(id) {
+  document.querySelector(`.window[data-id="${id}"]`).remove()
+  document.querySelector(`.tab[data-id="${id}"]`).remove()
+  
+  const index = open_windows.indexOf(id)
+  if (index > -1) {
+    open_windows.splice(index, 1)
+  }
+}
+
+function click() {
+  a_click.play()
+}
